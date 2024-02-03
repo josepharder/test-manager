9a9c2868dab8116dc4f27962d163dadcd298b7b2
joseharder13t@gmail.com <72845016+josepharder@users.noreply.github.com>
2024-02-03 02:35:35 -0600

    SW-9: init setup vue / eslint / ts config

---
+++ .modernizrrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    'feature-detects': [
+        'test/canvas',
+        'test/css/calc',
+        'test/css/cssgrid',
+        'test/css/flexbox',
+        'test/es6/promises',
+        'test/es7/array',
+        'webgl'
+    ]
+};
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,61 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
+/* eslint-disable */
+const path = require('path');
+const WorkerPlugin = require('worker-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = {
+  productionSourceMap: false,
+  transpileDependencies: ['multi-download'],
+  configureWebpack: {
+    plugins: [
+      new WorkerPlugin(),
+      new ForkTsCheckerWebpackPlugin({
+        typescript: {
+          extensions: {
+            vue: true
+          }
+        }
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@ant-design/icons/lib/dist$': path.resolve(__dirname, './src/icons.ts'),
+        modernizr$: path.resolve(__dirname, './.modernizrrc.js')
+      }
+    },
+    module: {
+      rules: [
+        {
+          loader: 'webpack-modernizr-loader',
+          test: /\.modernizrrc\.js$/
+        }
+      ]
+    },
+    optimization: {
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 100000,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+              return `npm.${packageName.replace('@', '')}`;
+            }
+          }
+        }
+      }
+    }
+  },
+  css: {
+    loaderOptions: {
+      less: {
+        lessOptions: {
+          javascriptEnabled: true
+        }
+      }
+    }
+  }
+};
